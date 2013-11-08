ab88f4f78a1a4ad48fb7a2ab8aa6d86810a6febb
Jonathan Pirnay <mail@johnnycrab.com>
2013-11-08 17:55:19 +0100

    encoding

--- html-to-pdf/main.py
+++ html-to-pdf/main.py
@@ -8,8 +8,16 @@ ps.psubscribe(["new:printable:*"])
 
 for item in ps.listen():
 
-	filename = item['channel'].replace('new:printable:', '')
+	channel = item['channel'].replace('new:printable:', '')
+	# split on hash to see if we have encoding
+	l = channel.split('#')
+	encoding = 'utf8'
+	filename = l[0]
+	if len(l) == 2:
+		encoding = l[1]
+
 	if filename != "*":
+
 		data = item['data']
 		# save to html file
 		savePath = htmlPath + filename + '.html'
@@ -20,5 +28,6 @@ for item in ps.listen():
 		except:
 			pass
 
-		args = ["phantomjs", "phantom.js", filename]
+		encoding_opt = '--output-encoding=' + encoding
+		args = ["phantomjs", encoding_opt, "phantom.js", filename]
 		p = subprocess.Popen(args)
